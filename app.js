1979716b14e51c1aaf1134ff7008f7fc01a81a69
Bevz Vyacheslav <paracord9@ukr.net>
2020-06-20 17:41:48 +0300

    install morgan and cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
@@ -8,9 +10,10 @@ const positionRoutes = require('./routes/position')
 const app = express()
 
 
+app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-
+app.use(cors())
 
 app.use('/api/auth' , authRoutes)
 app.use('/api/analytics' , analyticsRoutes)
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -4,6 +4,10 @@ var express = require('express');
 
 var bodyParser = require('body-parser');
 
+var cors = require('cors');
+
+var morgan = require('morgan');
+
 var authRoutes = require('./routes/auth');
 
 var analyticsRoutes = require('./routes/analytics');
@@ -15,10 +19,12 @@ var orderRoutes = require('./routes/order');
 var positionRoutes = require('./routes/position');
 
 var app = express();
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
